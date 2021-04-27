86a5650f9785892d633ffaa31e19b9b452c24481
Ivan <37513645+Yourhate419@users.noreply.github.com>
2021-04-27 13:24:44 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static int BinarySearch(int[] array, int searchedValue, int first, int last)
+    {
+        if (first > last)
+        {
+            return -1;
+        }
+
+        var middle = (first + last) / 2;
+
+        var middleValue = array[middle];
+
+        if (middleValue == searchedValue)
+        {
+            return middle;
+        }
+        else
+        {
+            if (middleValue > searchedValue)
+            {
+                return BinarySearch(array, searchedValue, first, middle - 1);
+            }
+            else
+            {
+                return BinarySearch(array, searchedValue, middle + 1, last);
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите элементы массива: ");
+
+        var s = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+        var array = new int[s.Length];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            array[i] = Convert.ToInt32(s[i]);
+        }
+
+        Array.Sort(array);
+        Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", array));
+
+        while (true)
+        {
+            Console.Write("Введите искомое значение: ");
+
+            var k = Convert.ToInt32(Console.ReadLine());
+            var searchResult = BinarySearch(array, k, 0, array.Length - 1);
+
+            if (searchResult < 0)
+            {
+                Console.WriteLine("Элемент со значением {0} не найден", k);
+            }
+            else
+            {
+                Console.WriteLine("Элемент найден. Индекс элемента со значением {0} равен {1}", k, searchResult);
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
